0a165c17e9198b22dfa37d5d26f3db8365efb49c
David <74916391+DavidShulga@users.noreply.github.com>
2022-01-18 17:10:52 +0300

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"61e6c66fd5b4680020a5b7df",
+				"61e6c66fd5b4680020a5b7e2"
+			]
+		},
+		"61e6c66fd5b4680020a5b7df": {
+			"id": "61e6c66fd5b4680020a5b7df",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"61e6c66fd5b4680020a5b7e2": {
+			"id": "61e6c66fd5b4680020a5b7e2",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
